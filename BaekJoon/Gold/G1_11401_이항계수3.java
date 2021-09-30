23e41c1a65a57400668f05db368ffc6f54c9acb6
JungJung <55391944+Talia2019@users.noreply.github.com>
2021-09-30 15:20:46 +0900

    Update G1_11401_이항계수3.java

--- BaekJoon/Gold/G1_11401_이항계수3.java
+++ BaekJoon/Gold/G1_11401_이항계수3.java
@@ -4,37 +4,50 @@ import java.util.Scanner;
 
 public class Main_G1_11401_이항계수3 {
 
-	public static final long DIVNUM = 1000000007;
-	public static int n, k;
+	// 구하고자 하는것 = n!/(r!(n-r)!) mod p
+	// n! (r!(n-r)!)^-1 mod p
+	// (r!(n-r)!)mod p의 역원 구하기
+
+	// 페르마의 정리 : a^p = a mod p
+	// a^p mod p = a mod p
+	// a^(p-1) mod p = 1 mod p
+	// a * a^(p-2) mod p = 1 mod p
+	// 즉 a mod p 의 역원은 a^(p-2)mod p
+
+	// (n! * (r!(n-r)!)^(p-2)mod p) mod p
+	// = (n! mod p * (r!(n-r)!)^(p-2)mod p) mod p
+
+	public static final int MOD = 1000000007;
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		k = sc.nextInt();
 
-		long up = factorial(n);
-		long bot = factorial(k) * factorial(n - k) % DIVNUM;
-		System.out.println(up * power(bot, DIVNUM - 2) % DIVNUM);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		long top = factorial(n);
+		long bot = (factorial(k) * factorial(n - k)) % MOD;
+		System.out.println((top * power(bot, MOD - 2)) % MOD);
 	}
 
-	public static long factorial(long n) {
-		long fac = 1L;
+	public static long factorial(int n) {
+		long res = 1;
 		while (n > 1) {
-			fac = (fac * n) % DIVNUM;
-			n--;
+			res = (res * n--) % MOD;
 		}
-		return fac;
+		return res;
 	}
 
-	public static long power(long n, long k) {
-		if (k == 1)
-			return n;
+	public static long power(long a, int n) {
+		if (n == 1)
+			return a;
 
-		long half = power(n, k / 2);
-		
-		if (k % 2 == 0)
-			return (half * half) % DIVNUM;
-		else
-			return (((half * half) % DIVNUM) * n % DIVNUM) % DIVNUM;
+		long tmp = power(a, n / 2);
+
+		if (n % 2 != 0)
+			return ((tmp * tmp) % MOD * a) % MOD;
+		return (tmp * tmp) % MOD;
 	}
+
 }
