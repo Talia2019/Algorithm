95c489bba509605dd2c0c0be60e4ce8cafc7e224
Talia <55391944+Talia2019@users.noreply.github.com>
2022-09-18 19:22:56 +0900

    Create G5_2410_2의멱수의합.java

---
+++ BaekJoon/Gold/G5_2410_2의멱수의합.java
@@ -0,0 +1,35 @@
+package CJY.week14;
+
+import java.util.Scanner;
+
+public class G5_2410 {
+    //G5_2410_2의멱수의합
+    //dp 안 쓰니 시간초과
+
+    //dp[k] 는 k를 2의 멱급수로 나타내는 경우의수
+    //for(0~n) dp[k] += dp[k-2^i]
+    //이때 n은 입력된수 이하의 최대2의제곱수
+
+    public static int MOD = 1000000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+
+        int mul = 1;
+        while(mul < n){
+            for (int i = 0; i <= n; i++) {
+                if(i >= mul){
+                    dp[i] = (dp[i] + dp[i-mul]) % MOD;
+                }
+            }
+            mul *= 2;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
+ 
